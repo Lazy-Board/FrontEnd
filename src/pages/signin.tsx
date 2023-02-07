14c12874c64eb3b48e8354911a652ef6f3fca2b6
test12270 <tykim1227@gmail.com>
2023-02-07 20:52:28 +0900

    소셜 로그인 변경

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,30 +1,29 @@
-import google from "../assets/google.svg";
+import google from "../assets/google.png";
 import kakao from "../assets/kakao.png";
 import naver from "../assets/naver.png";
 
 export default function Signin() {
   return (
     <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-      <span className="flex items-center mb-16 text-2xl font-semibold text-gray-900 dark:text-white">
-        로그인
-      </span>
-      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700 h-auto">
-        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+      <div className="w-1/3 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700 h-screen">
+        <div className="p-6 space-y-4 md:space-y-6 sm:p-8 my-24">
+          <span className="items-center mb-16 text-2xl font-semibold text-gray-900 dark:text-white">
+            로그인
+          </span>
           <form className="space-y-4 md:space-y-6" action="#">
+            <label className="block mb-4 text-sm font-medium text-gray-900 dark:text-white text-left">
+              이메일
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="name@email.com"
+            />
+
             <div>
-              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white text-left">
-                이메일
-              </label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="name@email.com"
-              />
-            </div>
-            <div>
-              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white text-left">
+              <label className="block mb-4 text-sm font-medium text-gray-900 dark:text-white text-left">
                 비밀번호
               </label>
               <input
@@ -44,12 +43,12 @@ export default function Signin() {
             </button>
             <div className="divider">간편 로그인</div>
             <div className="flex justify-center">
-              <img src={google} className="mr-12 w-10" />
-              <img src={kakao} className="mr-12 w-10" />
-              <img src={naver} className="w-10" />
+              <img src={google} />
             </div>
-            <div className="flex justify-center">
-              <span>회원가입 | 비밀번호 찾기</span>
+            <div className="flex justify-center text-gray-400">
+              <span className="mr-4">회원가입</span>
+              <span>|</span>
+              <span className="ml-4">비밀번호 찾기</span>
             </div>
           </form>
         </div>
