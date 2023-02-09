f3fd91841caac8060b05a8971556c138cb6d9e4b
test12270 <tykim1227@gmail.com>
2023-02-09 20:05:12 +0900

    signup api

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import Signup from "./pages/Signup";
+import { RecoilRoot } from "recoil";
 
 function App() {
-  return <Signup />;
+  return (
+    <RecoilRoot>
+      <Signup />
+    </RecoilRoot>
+  );
 }
 
 export default App;
