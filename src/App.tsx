e3964c4668a3897ac8e9b77176898fe558c83cc6
test12270 <tykim1227@gmail.com>
2023-02-14 17:13:32 +0900

    오류 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import Signin from "./pages/signin";
+import Signin from "./pages/Signin";
 
 function App() {
   const [count, setCount] = useState(0);
