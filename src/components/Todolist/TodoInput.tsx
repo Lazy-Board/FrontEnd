7df9e27dacff19d96d8aa9c7dd63b0fb842554ff
test12270 <tykim1227@gmail.com>
2023-02-24 19:53:04 +0900

    TodoList BE 연동 테스트코드

--- src/components/Todolist/TodoInput.tsx
+++ src/components/Todolist/TodoInput.tsx
@@ -1,4 +1,10 @@
-import React, { ChangeEvent, useCallback, KeyboardEvent } from "react";
+import axios from "axios";
+import React, {
+  ChangeEvent,
+  useCallback,
+  KeyboardEvent,
+  FormEvent,
+} from "react";
 
 import { FaPen } from "react-icons/fa";
 import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
@@ -13,29 +19,19 @@ const TodoInput = () => {
   // useRecoilValue = get 변수
   // useSetRecoilState = setter 지정
   // 위와 같은형식으로 get과 setter를 분리하여 사용하는 방법도 있습니다.
-
-  const addTodo = useCallback((): void => {
-    if (!contents.trim()) {
-      // 빈칸 입력 방지
-      return;
-    }
-
-    const nextId: number =
-      todos.length > 0 ? todos[todos.length - 1].id + 1 : 0;
-    // 배열에 값이 존재할시, 고유값은 마지막 인덱스에 위치한 id값에서 1을 늘려줘서 고유값 생성.
-    // 만약 값이 존재하지 않을시 초기값은 0
-
-    const todo: ITodoTypes = {
-      id: nextId,
-      contents,
-      isCompleted: false,
-    };
-
-    setTodos([...todos, todo]);
-    // 기존 객체들 복사 및 새로운 객체 추가
-
-    setContents("");
-  }, [contents, setContents, setTodos, todos]);
+  const addTodo = useCallback(
+    async (e: FormEvent) => {
+      e.preventDefault();
+      const res = await axios.post("/api/todos", { content: contents });
+      setTodos([...todos, res.data]);
+      setContents("");
+      if (!contents.trim()) {
+        // 빈칸 입력 방지
+        return;
+      }
+    },
+    [contents, setContents, setTodos, todos]
+  );
 
   const onChange = useCallback(
     (e: ChangeEvent<HTMLInputElement>): void => {
@@ -46,9 +42,9 @@ const TodoInput = () => {
   );
 
   const onKeyDown = useCallback(
-    (e: KeyboardEvent<HTMLInputElement>): void => {
+    (e: KeyboardEvent<HTMLInputElement>) => {
       if (e.key === "Enter") {
-        addTodo();
+        addTodo(e);
       }
     },
     [addTodo]
