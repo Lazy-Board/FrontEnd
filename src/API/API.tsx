0b1a3e6e61a41b6eb0629ce1171179e74cccbb00
test12270 <tykim1227@gmail.com>
2023-02-26 19:20:28 +0900

    Stock_#Feat:40 - Stock 수정

--- src/API/API.tsx
+++ src/API/API.tsx
@@ -1,1 +1,5 @@
-export const API_URL = "http://localhost:5173";
+// export const API_URL = "http://3.35.129.231:8080";
+// //서버 배포 URL
+
+export const API_URL = "http://localhost:3000";
+// 로컬 테스트 URL
