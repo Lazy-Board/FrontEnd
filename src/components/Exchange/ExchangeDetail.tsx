21a23f7faa15aad41eaed08816ecdf5a12948850
DESKTOP-3F2KJFP\delau <ark3dia@gmail.com>
2023-03-01 21:34:22 +0900

    test

--- src/components/Exchange/ExchangeDetail.tsx
+++ src/components/Exchange/ExchangeDetail.tsx
@@ -1,7 +1,7 @@
 import Accordion from "./Accordion";
 import styled from "styled-components";
 import { useState } from "react";
-import { useRecoilValue } from "recoil";
+import { useRecoilValueLoadable } from "recoil";
 import { getExchangeDetail, AccordionProps } from "../../atom/exchange";
 import DetailTopBar from "../MenuBars/DetailTopBar";
 
@@ -12,7 +12,8 @@ const Content = styled.div`
 `;
 
 const ExchangeDetail = (): JSX.Element => {
-  const data = useRecoilValue<AccordionProps[]>(getExchangeDetail);
+  const dataLoadable = useRecoilValueLoadable<AccordionProps[]>(getExchangeDetail);
+  let data='hasValue' === dataLoadable.state ? dataLoadable.contents : [];
   const [openAccordionIndex, setOpenAccordionIndex] = useState<number>(-1);
 
   return (
@@ -20,13 +21,19 @@ const ExchangeDetail = (): JSX.Element => {
       <DetailTopBar title="환율" />
       <Content className="max-w-md bg-stone-100 p-3">
         <div className="mt-16 text-left text-sm">
+          {dataLoadable.state === 'loading' ? <div>Loading...</div> :
+          <>
           <p>고시일시</p>
           <p>
             {data[0].updateAt.replace("년", "년 ").replace("월", "월 ")}
             &nbsp;({data[0].round})
           </p>
+          </>
+          }
         </div>
         <div className="w-full h-fit mt-5 p-2 pt-1 border border-slate-300 rounded-lg bg-white">
+          {dataLoadable.state === 'loading' ? <div>Loading...</div> : 
+          <>
           {data.map((item:any, index:any) => (
             <Accordion
               classes={index === data.length - 1 ? "pb-0" : "border-b pb-0"}
@@ -40,6 +47,8 @@ const ExchangeDetail = (): JSX.Element => {
               key={item.countryName}
             />
           ))}
+          </>
+          }
         </div>
       </Content>
     </>
