36c05baa70873c8bb0ff0921c72811b23c4e18c4
DESKTOP-3F2KJFP\delau <ark3dia@gmail.com>
2023-03-08 02:53:38 +0900

    dev_#Feat:81-프로필 이미지 로드 수정

--- src/components/User/UserProfile.tsx
+++ src/components/User/UserProfile.tsx
@@ -27,10 +27,11 @@ const UserProfile = (): JSX.Element => {
       <DetailTopBar title={"계정 관리"} />
       <Content className="max-w-md bg-stone-100 flex items-center content-center">
         <div className="my-24 mx-auto">
-          <img
-            src="/images/user-icon.png"
-            alt="프로필 이미지"
-            className="w-24 h-24 mx-auto bg-gray-300 rounded-full"
+        <img src={isLoading ? '/images/user-icon.png' : 
+          !userInfo.profile ? '/images/user-icon.png' :
+          userInfo.profile} 
+              alt="프로필 이미지" 
+              className="w-24 h-24 mx-auto bg-gray-300 rounded-full object-cover"
           />
           {isLoading ? (
             <div className="mx-auto">Loading...</div>
