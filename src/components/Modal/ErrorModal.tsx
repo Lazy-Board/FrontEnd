c7e9568220c72ea901c3e4b5527c88f7e91fd532
tae young kim <63242552+test12270@users.noreply.github.com>
2023-03-07 20:49:07 +0900

    Update ErrorModal.tsx

--- src/components/Modal/ErrorModal.tsx
+++ src/components/Modal/ErrorModal.tsx
@@ -1,30 +1,6 @@
 import { BiErrorCircle } from "react-icons/bi";
 import styled from "styled-components";
 
-<<<<<<< HEAD
-export const Modal = (props: any) => {
-  return (
-    <div className="fixed z-10 inset-0 overflow-y-auto">
-      <div className="flex items-center justify-center min-h-screen">
-        <div className="bg-white p-6 rounded-lg shadow-lg border border-slate-300 w-72">
-          <div className="flex items-center">
-            <BiMessageError fill="red" size={20} />
-            <h2 className="text-lg font-medium text-gray-900 ml-2 pb-2">
-              {props.title}
-            </h2>
-          </div>
-          <div className="mt-4">
-            <p className="text-sm text-gray-500">{props.message}</p>
-          </div>
-          <div className="mt-4">
-            <button
-              className="px-4 py-2 bg-primary text-white rounded hover:bg-green-700"
-              onClick={props.onClose}
-            >
-              Close
-            </button>
-          </div>
-=======
 const Max=styled.p`
     max-width:215px;
 `
@@ -49,7 +25,6 @@ export const ErrorModal = (props: any) => {
                     </button>
                 </div>
             </div>
->>>>>>> 5b76d44bcdd4d60021bfc8d521b54df254d7197a
         </div>
         </div>
     );
