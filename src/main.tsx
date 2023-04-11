f898be7be99f6ef7f8c27bf0f675fadfc13f449c
tae young kim <63242552+test12270@users.noreply.github.com>
2023-04-11 17:56:49 +0900

    Create main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-<<<<<<< HEAD
-import { RecoilRoot } from "recoil";
-import App from "./App";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <RecoilRoot>
-      <App />
-=======
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { RecoilRoot } from "recoil";
@@ -23,7 +13,6 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
       <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
->>>>>>> ede9d980749506e433fa4e99e757901c588ed040
     </RecoilRoot>
   </React.StrictMode>
 );
