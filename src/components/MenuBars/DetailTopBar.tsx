95eeb114245808e3409b7367c7fffe1757c67722
Pianos <121603759+lifeIsPiano@users.noreply.github.com>
2023-04-11 21:40:00 +0900

    Update DetailTopBar.tsx

--- src/components/MenuBars/DetailTopBar.tsx
+++ src/components/MenuBars/DetailTopBar.tsx
@@ -22,14 +22,14 @@ const DetailTopBar = ({ title }: any) => {
   const locationNow = useLocation();
 
   const goHome = () => {
-    if (locationNow.pathname === 'exchange' || locationNow.pathname === 'stock') {
+    if (locationNow.pathname === '/exchange' || locationNow.pathname === '/stock') {
       navigate("/");
       location.reload();
     } else {
       navigate(-1);
     }
   };
-
+  
   return (
     <Top className="h-14 flex items-center justify-center bg-white dark:bg-neutral shadow-md dark:shadow-slate-700 dark:text-slate-100">
       <button
