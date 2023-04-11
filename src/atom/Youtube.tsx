99277eb3814e22a8f07717e5fb80a2bc59cc0e30
tae young kim <63242552+test12270@users.noreply.github.com>
2023-04-11 17:57:34 +0900

    Update Youtube.tsx

--- src/atom/Youtube.tsx
+++ src/atom/Youtube.tsx
@@ -1,16 +1,6 @@
 import axios from "axios";
 import { selector } from "recoil";
 import { API_URL } from "../API/API";
-<<<<<<< HEAD
-
-export const getYoutube = selector({
-  key: "getYoutube",
-  get: async ({ get }) => {
-    const response = await axios.get(`${API_URL}/youtube`);
-    return response.data;
-  },
-});
-=======
 import { api } from "./signin";
 
 export const getYoutube = selector<YoutubeProps[]>({
@@ -32,4 +22,3 @@ export type YoutubeProps = {
   videoId: string;
   videoUrl: string;
 };
->>>>>>> ede9d980749506e433fa4e99e757901c588ed040
