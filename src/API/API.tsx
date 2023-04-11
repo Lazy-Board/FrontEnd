c99aca56e4a0ff66037e904c8b4ca120b9bb6a8c
tae young kim <63242552+test12270@users.noreply.github.com>
2023-04-11 17:58:18 +0900

    Update API.tsx

--- src/API/API.tsx
+++ src/API/API.tsx
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-// export const API_URL = "http://3.35.129.231:8080";
-//서버 배포 URL
-
-export const API_URL = "http://localhost:3000";
-=======
 export const API_URL = "http://3.34.73.141:8080";
 //서버 배포 URL
 
 // export const API_URL = "http://localhost:3000";
->>>>>>> ede9d980749506e433fa4e99e757901c588ed040
 //로컬 테스트 URL
